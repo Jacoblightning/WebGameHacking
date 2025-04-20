026cd82e0cb7f2a12ecee6c08b32a49bc3f408cb
Jacob Freeman <t00niyf5@duck.com>
2025-04-20 16:37:16 -0400

    SuperGenomic: Respect cancel button

--- Scripts/SuperGenomic/SuperGenomic.js
+++ Scripts/SuperGenomic/SuperGenomic.js
@@ -9,32 +9,69 @@ let MUTATION = null;
 // Maximum possible deletion. Normal values: level 1 : 2,   level 2 : 3,   level 3 : 4
 let DELETION = null;
 
+// Auto config
+{
+    let canceled = false;
 
-// Prompt user for LENGTH if needed
-while (LENGTH === null) {
-    let tmp1 = prompt("You did not specify a length in the config. What would you like the length of the sequence to be?", "50");
-    let tmp2 = Number.parseInt(tmp1);
-    if (!Number.isNaN(tmp2)) {
-        LENGTH = tmp2;
-        break;
+    // We don't check for canceled here as it is the first one and tne user can't have pressed cancel yet.
+    // Prompt user for LENGTH if needed
+    while (LENGTH === null) {
+        let tmp1 = prompt("You did not specify a length in the config. What would you like the length of the sequence to be?", "50");
+        // If the user pressed cancel
+        if (tmp1 === null) {
+            LENGTH = 50;
+            canceled = true;
+            break;
+        }
+
+        let tmp2 = Number.parseInt(tmp1);
+        if (!Number.isNaN(tmp2)) {
+            LENGTH = tmp2;
+            break;
+        }
     }
-}
-// Prompt user for MUTATION if needed
-while (MUTATION === null) {
-    let tmp1 = prompt("You did not specify a mutation probability in the config. What would you like the mutation probability to be?", "50%");
-    let tmp2 = Number.parseInt(tmp1);
-    if (!Number.isNaN(tmp2)) {
-        MUTATION = tmp2;
-        break;
+    // If the user canceled, then we skip the rest of the interactivity
+    if (!canceled) {
+        // Prompt user for MUTATION if needed
+        while (MUTATION === null) {
+            let tmp1 = prompt("You did not specify a mutation probability in the config. What would you like the mutation probability to be?", "50%");
+            // If the user pressed cancel
+            if (tmp1 === null) {
+                MUTATION = 50;
+                canceled = true;
+                break;
+            }
+
+            let tmp2 = Number.parseInt(tmp1);
+            if (!Number.isNaN(tmp2)) {
+                MUTATION = tmp2;
+                break;
+            }
+        }
+    } else {
+        // We just set it to the default value if the user canceled
+        MUTATION = 50;
     }
-}
-// Prompt user for DELETION if needed
-while (DELETION === null) {
-    let tmp1 = prompt("You did not specify a maximum deletion length in the config. What would you like the maximum deletion length to be?", "5");
-    let tmp2 = Number.parseInt(tmp1);
-    if (!Number.isNaN(tmp2)) {
-        DELETION = tmp2;
-        break;
+    // Same as above
+    if (!canceled) {
+        // Prompt user for DELETION if needed
+        while (DELETION === null) {
+            let tmp1 = prompt("You did not specify a maximum deletion length in the config. What would you like the maximum deletion length to be?", "5");
+            // If the user pressed cancel
+            if (tmp1 === null) {
+                LENGTH = 50;
+                canceled = true;
+                break;
+            }
+
+            let tmp2 = Number.parseInt(tmp1);
+            if (!Number.isNaN(tmp2)) {
+                DELETION = tmp2;
+                break;
+            }
+        }
+    } else {
+        DELETION = 5;
     }
 }
 
